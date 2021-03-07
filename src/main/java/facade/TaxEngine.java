70b1b4196df137de0c84890b5962e3705e8caac2
ch <mkmsacc@outlook.com>
2021-03-07 16:18:46 +0700

    Add TaxEngine

---
+++ src/main/java/facade/TaxEngine.java
@@ -0,0 +1,14 @@
+package facade;
+
+import strategy.Money;
+
+public class TaxEngine {
+    public TaxEngine(Configuration configuration){
+        // code to create objects, configure, wire thing up ...
+    }
+
+    public Money calculateTaxLiability(DateRange taxPeriod) {
+        // code ...
+        return null;
+    }
+}
